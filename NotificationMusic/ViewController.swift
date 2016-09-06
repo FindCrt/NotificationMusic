4de6af8d207ba3d919fc672d0515c12338c188c4
1030232848@qq.com <1030232848@qq.com>
2016-09-06 16:42:45 +0800

    Initial Commit

---
+++ NotificationMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationMusic
+//
+//  Created by shiwei on 16/9/6.
+//  Copyright © 2016年 shiwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
